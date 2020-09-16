cbc8254d371564b1d3377ddee3dea63a9f6cae52
Chris Olsen <olsen.chris@gmail.com>
2020-09-15 20:32:45 -0600

    move the landing page to it's own activity

--- app/src/main/java/org/chrisolsen/mathops/views/landing/LandingFragment.kt
+++ app/src/main/java/org/chrisolsen/mathops/views/landing/LandingFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil.inflate
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
 import org.chrisolsen.mathops.R
 import org.chrisolsen.mathops.databinding.LandingFragmentBinding
 
@@ -19,15 +17,6 @@ class LandingFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = inflate(inflater, R.layout.landing_fragment, container, false)
-
-        binding.getStarted.setOnClickListener { view: View ->
-            view.findNavController()
-                .navigate(
-                    LandingFragmentDirections.actionLandingFragmentToGameOptionsFragment(),
-                )
-        }
-        activity?.title = ""
-        return binding.root
+        return inflater.inflate(R.layout.landing_fragment, container, false)
     }
 }
